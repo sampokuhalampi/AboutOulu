a57611ca9c476bc22c9a016f4a5930c993583f64
Antte Heatta <aheatta@hotmail.com>
2022-04-29 00:39:39 +0300

    update main page

--- Main Screen/script.js
+++ Main Screen/script.js
@@ -7,11 +7,29 @@ button.onClick = infoNappi () ({
 })
 
 function ravintolat() {
+    location.href = "/Kategoriat/Ravintolat/ravintolat.html"
+}
+
+button.onClick = ravintolat () ({
+
+})
+
+function nahtavyydet() {
     // location.href = "/Ravintolat/index.html"
     location.href = "/Info-sivu/infoPage.html"
 
 }
 
-button.onClick = ravintolat () ({
+button.onClick = nahtavyydet () ({
+
+})
+
+function aktiviteetit() {
+    // location.href = "/Ravintolat/index.html"
+    location.href = "/Info-sivu/infoPage.html"
+
+}
+
+button.onClick = aktiviteetit () ({
 
 })
