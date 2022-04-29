f79a370f681e426dc7f27106585dc758347a122e
Sampo <sampo.kuhalampi@icloud.com>
2022-04-29 11:54:13 +0300

    toimii kohta

---
+++ Main Screen/main.js
@@ -0,0 +1,32 @@
+function infoNappi () {
+    location.href = "/Info-sivu/infoPage.html"
+}
+
+button.onClick = infoNappi () ({
+
+})
+
+function ravintolat() {
+    location.href = "/Kategoriat/Ravintolat/ravintolat.html"
+}
+
+button.onClick = ravintolat () ({
+
+})
+
+function nahtavyydet() {
+    location.href = "/Kategoriat/Nähtävyydet/nahtavyydet.html"
+
+}
+
+button.onClick = nahtavyydet () ({
+
+})
+
+function hotellit() {
+    location.href = "/Kategoriat/Hotellit/hotellit.html"
+}
+
+    button.onClick = aktiviteetit () ({
+
+})
