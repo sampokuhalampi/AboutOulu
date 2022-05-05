b292932ae3f34ec9ee31530099949dd38e2877b2
Antte Heatta <aheatta@hotmail.com>
2022-05-05 14:51:43 +0300

    kohteenTemplate undone

--- Kohteet/Kohde1/kohteenTemplateScript.js
+++ Kohteet/Kohde1/kohteenTemplateScript.js
@@ -1,8 +1,36 @@
-function takaisin () {
-    location.href = "/Main Screen/main.html"
+function render(data) {
+  var html =
+    "<div class='commentBox'><div class='leftPanelImg'></div><div class='rightPanel'><span>" +
+    data.name +
+    "</div><p>" +
+    data.body +
+    "</p></div><div class='clear'></div></div>";
+
+  $("#container").append(html);
 }
 
-button.onClick = takaisin () ({
-    
-})
+$(document).ready(function () {
+  var comment = [{ name: "Testikäyttäjä", body: "Ihan hieno paikka" }];
 
+  for (var i = 0; i < comment.length; i++) {
+    render(comment[i]);
+  }
+
+  $("#addComment").click(function () {
+    var addObj = {
+      name: $("#name").val(),
+      body: $("#bodyText").val(),
+    };
+
+    comment.push(addObj);
+    render(addObj);
+    $("#name").val("");
+    $("#bodyText").val("");
+  });
+});
+
+function takaisin() {
+  location.href = "/Main Screen/main.html";
+}
+
+button.onClick = takaisin()({});
