82be73d26fb7ae4b0a1b47824ac81763b156dec9
Antte Heatta <aheatta@hotmail.com>
2022-05-11 18:53:51 +0300

    update

--- Main Screen/main.js
+++ Main Screen/main.js
@@ -1,32 +1,33 @@
-function infoNappi () {
-    location.href = "/Info-sivu/infoPage.html"
+function infoNappi() {
+  location.href = "/Info-sivu/infoPage.html";
 }
-
-button.onClick = infoNappi () ({
-
-})
 
 function ravintolat() {
-    location.href = "/Kategoriat/Ravintolat/ravintolat.html"
+  location.href = "/Kategoriat/Ravintolat/ravintolat.html";
 }
-
-button.onClick = ravintolat () ({
-
-})
 
 function nahtavyydet() {
-    location.href = "/Kategoriat/Nähtävyydet/nahtavyydet.html"
-
+  location.href = "/Kategoriat/Nähtävyydet/nahtavyydet.html";
 }
-
-button.onClick = nahtavyydet () ({
-
-})
 
 function hotellit() {
-    location.href = "/Kategoriat/Hotellit/hotellit.html"
+  location.href = "/Kategoriat/Hotellit/hotellit.html";
 }
 
-    button.onClick = aktiviteetit () ({
+function textSearch() {
+  var input, uppercase, ul, li, a, i, text;
+  input = document.getElementById("searchBox");
+  uppercase = input.value.toUpperCase();
+  ul = document.getElementById("listOfDestinations");
+  li = ul.getElementsByTagName("li");
 
-})
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    text = a.textContent || a.innerText;
+    if (text.toUpperCase().indexOf(uppercase) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
