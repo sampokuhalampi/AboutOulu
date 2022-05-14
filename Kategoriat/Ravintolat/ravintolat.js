cb1e7431253520ee6a6ceb8ff4ad184d4709a839
sampokuhalampi <sampo.kuhalampi@icloud.com>
2022-05-14 09:28:14 +0300

    valmis

--- Kategoriat/Ravintolat/ravintolat.js
+++ Kategoriat/Ravintolat/ravintolat.js
@@ -1,6 +1,8 @@
 function returnToMain() {
-  location.href = "/index.html";
+  location.href = "/Main Screen/main.html";
 }
+
+//button.onClick = takaisin()({});
 
 function search() {
   console.log("hae painettu");
@@ -88,6 +90,7 @@ function search() {
         "group",
         "disabled",
         "diverse",
+        "comfy",
         "snack",
         "everyone",
         "comfy",
